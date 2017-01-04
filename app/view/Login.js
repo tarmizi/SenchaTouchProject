9969a16b864a8e5cdc1e23e7a977bd2d6e3dab94
tarmizi <tarmizi_09@hotmail.my>
2017-01-04 23:26:44 +0800

    revert the original login.js

--- app/view/Login.js
+++ app/view/Login.js
@@ -87,7 +87,7 @@ Ext.define('MyGPS.view.Login', {
                 {
                     xtype: 'textfield',
                     //  margin: '-10 0 0 0',
-                    placeHolder: 'Put Username',
+                    placeHolder: 'Username',
                     id: 'loginuserNameTextField',
                     // name: 'userNameTextFieldname',
                     required: true
